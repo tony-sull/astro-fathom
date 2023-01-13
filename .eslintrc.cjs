c57190027b9cc908f425fd840898f1fe626a35bf
Tony Sullivan <tony@astro.build>
2023-01-13 14:57:35 -0600

    WIP: adding linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,37 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-recommended",
+  ],
+  plugins: ["prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    extraFileExtensions: [".astro"],
+  },
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+    "@typescript-eslint/consistent-type-imports": ["error", { fixStyle: "inline-type-imports" }],
+    "@typescript-eslint/no-floating-promises": "warn",
+    "@typescript-eslint/no-redeclare": "warn",
+    "no-console": ["error", { allow: ["warn", "error", "info"] }],
+    "no-useless-rename": "error",
+    "object-shorthand": "error",
+  },
   overrides: [
     {
-      files: ['*.ts'],
-      parser: '@typescript-eslint/parser',
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        rules: {
-          indent: ['error', 2],
-          'linebreak-style': ['error', 'unix'],
-          quotes: ['error', 'single'],
-          semi: ['error', 'never'],
-        },
+        parser: "@typescript-eslint/parser",
       },
+      rules: {},
     },
   ],
 }
