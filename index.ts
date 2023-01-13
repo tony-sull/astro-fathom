6809f0a5587626d3460f9e74ece875ee45ad8ff5
Tony Sullivan <tony@astro.build>
2023-01-13 14:47:20 -0600

    no integration needed

--- index.ts
+++ index.ts
@@ -1,15 +1,1 @@
-import type { AstroIntegration } from 'astro'
-
-export default function createIntegration(): AstroIntegration {
-  // See the Integration API docs for full details
-  // https://docs.astro.build/en/reference/integrations-reference/
-  return {
-    name: '@example/my-integration',
-    hooks: {
-      'astro:config:setup': () => {
-        // See the @astrojs/react integration for an example
-        // https://github.com/withastro/astro/blob/main/packages/integrations/react/src/index.ts
-      },
-    },
-  }
-}
+export { default as Fathom } from "./Fathom.astro"
